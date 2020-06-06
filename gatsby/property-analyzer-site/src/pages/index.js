231fa67c1f076ccff7ef24c1d1d4dc23c13c72e4
remisharoon <remisharoon@gmail.com>
2020-06-06 23:37:41 +0400

    update homepage details

--- gatsby/property-analyzer-site/src/pages/index.js
+++ gatsby/property-analyzer-site/src/pages/index.js
@@ -16,11 +16,10 @@ export default () => (
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+            Propery Analyzer
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-            Free landing page template to promote your business startup and generate leads for the
-            offered services
+          Analyse the housing market and research individual properties from your desk. You'll find data on prices, price/sqft, rental yields, planning, boundaries & footprints and much more.
           </p>
           <p className="mt-8 md:mt-12">
             <Button size="lg">Get Started</Button>
