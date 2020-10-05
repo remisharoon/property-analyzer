1d1ae4ab9f206f29339eacbb12bdf3ce9018f515
ramiz <remisharoon@gmail.com>
2020-10-05 11:23:08 +0400

    adding dependency alasql for sql on JS

--- gatsby/property-analyzer-site/src/data/dubai_prop_prices.js
+++ gatsby/property-analyzer-site/src/data/dubai_prop_prices.js
@@ -1,3 +1,40 @@
+import * as alasql from 'alasql';
+
+// var timing_output = '[ {  "path" : "https:\/\/mail.google.com\/mail\/u\/0",    "endDate" : "2015-02-11, 1:00 PM",    "startDate" : "2015-02-11, 12:00 PM",    "application" : "Safari",    "duration" : 1528.618035137653  }]';
+
+// alasql('SELECT projects, duration, path, application \
+// FROM json("timing_output") \
+// GROUP BY projects, duration, path, application \
+// ORDER BY duration DESC \
+// ;\
+// ',[],function(res){
+//               console.log(res);
+//   });
+
+// const results = alasql(['SELECT * FROM CSV("./data/monthly_avg_price.csv", {headers:true})'])
+// .then(function(res){
+//   console.log(res); // output depends on mydata.xls
+// }).catch(function(err){
+//   console.log('Does the file exist? There was an error:', err);
+// });
+
+// console.log("results = " + results);
+
+// var data = [ {a: 1, b: 10}, {a: 2, b: 20}, {a: 1, b: 30} ];
+
+// var res = alasql('SELECT a, SUM(b) AS b FROM ? GROUP BY a',[data]);
+
+// console.log("resss = " + JSON.stringify(res, null, 2));
+
+alasql.promise('SELECT col1 FROM CSV("monthly_avg_price.csv", {headers:false, quote:"\'",separator:","})')
+.then(function(data){
+     console.log(data);
+    console.log("Gooooodoododdoododododdo");
+}).catch(function(err){
+      console.log("Erroooorororororororororo");
+    //  console.log('Error:', err);
+});
+
 export default [
     { name: 'January', uv: 4000, pv: 2400, amt: 2400 },
     { name: 'February', uv: 3000, pv: 1398, amt: 2210 },
