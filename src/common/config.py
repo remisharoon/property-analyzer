75cac1f554afac764dbb21107172dd6b61110209
remis <remisharoon@gmail.com>
2021-03-01 11:27:25 +0400

    adding dummy config file

--- src/common/config.py
+++ src/common/config.py
@@ -1,7 +1,22 @@
 import yaml
+import re
+import os
+
+path_matcher = re.compile(r'\$\{([^}^{]+)\}')
+def path_constructor(loader, node):
+  ''' Extract the matched value, expand env variable, and replace the match '''
+  value = node.value
+  match = path_matcher.match(value)
+  env_var = match.group()[2:-1]
+  return os.environ.get(env_var) + value[match.end():]
+
+yaml.add_implicit_resolver('!path', path_matcher)
+yaml.add_constructor('!path', path_constructor)
+
+
 try:
     with open("../config.yaml", "r") as ymlfile:
-        cfg = yaml.load(ymlfile)
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 except Exception as e:
     with open("../../config.yaml", "r") as ymlfile:
-        cfg = yaml.load(ymlfile)
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
