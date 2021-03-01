bcb00391c6ee9615913b7f3891ca1c2cc121d58b
remis <remisharoon@gmail.com>
2021-03-01 19:42:35 +0400

    use EnvYAML for reading env variables

--- src/common/config.py
+++ src/common/config.py
@@ -1,25 +1,39 @@
-import yaml
-import re
-import os
+# import yaml
+# import re
+# import os
+from envyaml import EnvYAML
 
-path_matcher = re.compile(r'\$\{([^}^{]+)\}')
-def path_constructor(loader, node):
-  ''' Extract the matched value, expand env variable, and replace the match '''
-  value = node.value
-  match = path_matcher.match(value)
-  env_var = match.group()[2:-1]
-  return os.environ.get(env_var) + value[match.end():]
+# path_matcher = re.compile(r'\$\{([^}^{]+)\}')
+# def path_constructor(loader, node):
+#   ''' Extract the matched value, expand env variable, and replace the match '''
+#   value = node.value
+#   match = path_matcher.match(value)
+#   env_var = match.group()[2:-1]
+#   return os.environ.get(env_var) + value[match.end():]
+#
+# yaml.add_implicit_resolver('!path', path_matcher)
+# yaml.add_constructor('!path', path_constructor)
 
-yaml.add_implicit_resolver('!path', path_matcher)
-yaml.add_constructor('!path', path_constructor)
+# try:
+#     with open("config.yaml", "r") as ymlfile:
+#         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+# except Exception as e:
+#     try:
+#         with open("../config.yaml", "r") as ymlfile:
+#             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+#     except Exception as e:
+#         with open("../../config.yaml", "r") as ymlfile:
+#             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 try:
-    with open("config.yaml", "r") as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    cfg = EnvYAML(yaml_file='config.yaml', strict=False)
 except Exception as e:
     try:
-        with open("../config.yaml", "r") as ymlfile:
-            cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+        cfg = EnvYAML('../config.yaml')
     except Exception as e:
-        with open("../../config.yaml", "r") as ymlfile:
-            cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+        cfg = EnvYAML('../../config.yaml')
+
+
+print(cfg['ibm_credentials.auth_endpoint'])
+ 
+# iam_serviceid_crn: ${IBM_SERVICEID_CRN}
