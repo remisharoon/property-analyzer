0f100cad02ba2ee3d3c4da47455cb8d3b9198ae8
remis <remisharoon@gmail.com>
2021-04-15 19:51:29 +0400

    KC and FAQ templates

--- gatsby/property-analyzer-site/src/pages/knowledge_center.js
+++ gatsby/property-analyzer-site/src/pages/knowledge_center.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react"
 import Layout from '../components/layout/Layout';
 import LabelText from '../components/LabelText';
 import CustomerCard from '../components/CustomerCard';
+// import Article from "../components/Article/";
+// import {Row, Col,} from 'antd';
+import SplitSection from '../components/SplitSection';
+import SvgCharts from '../svg/SvgCharts';
+
 import '../css/chart.css';
 
 export default function Knowledge() {
@@ -19,11 +24,75 @@ export default function Knowledge() {
             </div>      
           </section>
 
+
+
+          <div className="container mx-auto">
+              <div className="text-center lg:text-left lg:w-1/2">    
+                <button className="bg-green-600">Button</button>
+              </div>      
+          </div>      
+
+        <div className="container mx-auto px-16 items-center flex flex-col lg:flex-row">
+          <div className="lg:w-1/2">
+            <div className="lg:pl-32 xl:pl-48">
+                <h3 className="text-3xl font-semibold leading-tight">
+                  Design And Plan Your Business Growth Steps
+                </h3>
+                <p className="mt-8 text-xl font-light leading-relaxed">
+                <ul>
+                    <li>Deposits as per Agreement of sale between Buyer & Seller</li>
+                    <li>Transfer fees with the Land Department</li>
+                    <li>Connection fees for electricity and water authorities</li>
+                    <li>Community service fees</li>
+                    <li>Mortgage application fees if required</li>
+                    <li>Misc admin fees to the Land Department</li>
+                    <li>Commission</li>
+
+                    </ul>  
+                </p>
+              </div>            
+          </div>
+        </div>          
+
+
+        <div class="p-10">  
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          {/* <img class="w-full" src="/mountain.jpg" alt="Mountain"> */}
+          <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">Mountain</div>
+              <p class="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+              </p>
+              </div>
+            <div class="px-6 pt-4 pb-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+          </div>
+        </div>
+        
+
+      <div class="w-full sm:w-1/2 md:w-1/3">
+        <h1 class="font-sans font-thin mb-4">Article title</h1>
+
+        <p class="text-grey mb-3">Written by Walter Bishop on 25 May 2070. Posted in News</p>
+
+        <h2 class="font-sans font-thin leading-normal mb-4">The observers are coming. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</h2>
+
+        <p class="text-grey-darkest mb-6 leading-tight">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        <div>
+          <a href="#" class="text-black mr-2">READ MORE</a>
+          <a href="#" class="text-black">5 COMMENTS</a>
+        </div>
+
+      </div>
+
           <section id="testimonials" className="py-20 lg:py-40">
             <div className="container mx-auto">
                 <LabelText className="mb-6 text-gray-600 text-left">A guide to buying property in dubai</LabelText>
                 <div className="flex flex-col md:flex-row md:-mx-3">
-                    <p>
                     <ul>
                     <li>Deposits as per Agreement of sale between Buyer & Seller</li>
                     <li>Transfer fees with the Land Department</li>
@@ -34,7 +103,6 @@ export default function Knowledge() {
                     <li>Commission</li>
 
                     </ul>                          
-                    </p>
                   
                 </div>
             </div>
--- gatsby/property-analyzer-site/src/pages/qna.js
+++ gatsby/property-analyzer-site/src/pages/qna.js
@@ -7,36 +7,97 @@ import '../css/chart.css';
 export default function QnA() {
     return (
         <Layout>
-          <section className="pt-20 md:pt-40">
-            <div className="container mx-auto px-8 lg:flex">
-              <div className="text-center lg:text-left lg:w-1/2">    
-                <h3>Q & A Center</h3>
-
-                <p>Such wow. This is an awesome place to learn new stuff about property markets.</p>
-
-                
-              </div>      
-            </div>      
-          </section>
-
-          <section id="testimonials" className="py-20 lg:py-40">
+          <section id="faq" className="py-20 lg:py-40">
             <div className="container mx-auto">
-                <LabelText className="mb-6 text-gray-600 text-left">A guide to buying property in dubai</LabelText>
-                <div className="flex flex-col md:flex-row md:-mx-3">
-                    <p>
-                    <ul>
-                    <li>Deposits as per Agreement of sale between Buyer & Seller</li>
-                    <li>Transfer fees with the Land Department</li>
-                    <li>Connection fees for electricity and water authorities</li>
-                    <li>Community service fees</li>
-                    <li>Mortgage application fees if required</li>
-                    <li>Misc admin fees to the Land Department</li>
-                    <li>Commission</li>
 
-                    </ul>                          
-                    </p>
-                  
-                </div>
+
+              <div>
+                    <section class="text-gray-700">
+                      <div class="container px-5 py-24 mx-auto">
+                        <div class="text-center mb-20">
+                          <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+                            Frequently Asked Question
+                          </h1>
+                          <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                            The most common questions about how our business works and what can do for you. This is an awesome place to learn new stuff about property markets.
+                          </p>
+                        </div>
+                        <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                          <div class="w-full lg:w-1/2 px-4 py-2">
+                            <details class="mb-4">
+                              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                                How Long is this site live?
+                              </summary>
+
+                              <span>
+                                Laboris qui labore cillum culpa in sunt quis sint veniam.
+                                Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                                minim velit nostrud pariatur culpa magna in aute.
+                              </span>
+                            </details>
+                            <details class="mb-4">
+                              <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
+                                Can I install/upload anything I want on there?
+                              </summary>
+
+                              <span>
+                                Laboris qui labore cillum culpa in sunt quis sint veniam.
+                                Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                                minim velit nostrud pariatur culpa magna in aute.
+                              </span>
+                            </details>
+                            <details class="mb-4">
+                              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                                How can I migrate to another site?
+                              </summary>
+
+                              <span>
+                                Laboris qui labore cillum culpa in sunt quis sint veniam.
+                                Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                                minim velit nostrud pariatur culpa magna in aute.
+                              </span>
+                            </details>
+                          </div>
+                          <div class="w-full lg:w-1/2 px-4 py-2">
+                            <details class="mb-4">
+                              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                                Can I change the domain you give me?
+                              </summary>
+
+                              <span class="px-4 py-2">
+                                Laboris qui labore cillum culpa in sunt quis sint veniam.
+                                Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                                minim velit nostrud pariatur culpa magna in aute.
+                              </span>
+                            </details>
+                            <details class="mb-4">
+                              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                                How many sites I can create at once?
+                              </summary>
+
+                              <span class="px-4 py-2">
+                                Laboris qui labore cillum culpa in sunt quis sint veniam.
+                                Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                                minim velit nostrud pariatur culpa magna in aute.
+                              </span>
+                            </details>
+                            <details class="mb-4">
+                              <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                                How can I communicate with you?
+                              </summary>
+
+                              <span class="px-4 py-2">
+                                Laboris qui labore cillum culpa in sunt quis sint veniam.
+                                Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                                minim velit nostrud pariatur culpa magna in aute.
+                              </span>
+                            </details>
+                          </div>
+                        </div>
+                      </div>
+                    </section>
+                  </div>
+
             </div>
           </section>
   
